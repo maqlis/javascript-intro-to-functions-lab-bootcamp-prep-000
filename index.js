e89bfe52ed01bb6a097b4638109329f0f3dfbb91
Małgorzata Klisińska <klis.mal@gmail.com>
2017-10-22 08:20:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if string===whisper(string) return "I can\'t hear you!"
+  if(string===whisper(string)){
+     return "I can\'t hear you!"
+   }
 
 
 
